45b44936447260b58fc31c1c4e87ec060ef36ce7
iamVarjeet <iamkuljeet007@gmail.com>
2024-10-09 17:48:04 +0530

    commit

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { products } from "../../data/data";
 import "./Products.css";
 import Button from '../button/Button';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 export default function Products() {
+    const [isHovered, setIsHovered] = useState(null);
     return (
         <div className='products'>
             <div className="heading-box flex">
@@ -14,9 +16,14 @@ export default function Products() {
 
             <ul className='flex products-box'>
                 {products.map((product, id) => {
+
                     return (
                         <li key={id} className='product flex'>
-                            <div className='heart flex'>{product.icon} </div>
+                            <div onMouseEnter={() => setIsHovered(id)}
+                                onMouseLeave={() => setIsHovered(null)}
+                                className='heart flex'>
+                                {isHovered === id ? <AiFillHeart /> : <AiOutlineHeart />}
+                            </div>
                             <img src={product.imageSrc} alt="productImg" className='product-img' />
                             <h3>{product.title}</h3>
                         </li>
